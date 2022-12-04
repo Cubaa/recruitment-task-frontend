5872e087e65c26d67f98e31e02c8b493f3db5c31
Cubaa <jakub29-11@wp.pl>
2022-12-04 18:16:52 +0100

    move content and top bar folder to profile folder

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
-import { Content } from '../content/content'
-import { TopBar } from '../top-bar/top-bar'
+import { Content } from './content/content'
+import { TopBar } from './top-bar/top-bar'
 
 export const Profile: FC = () => {
   return (
